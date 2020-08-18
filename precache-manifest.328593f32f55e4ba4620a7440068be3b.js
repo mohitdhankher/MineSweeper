041e65a44603a4af3238cb8f724b6e74a61d90fd
Mohit Dhankher <Mohit.Dhankher@mu-sigma.com>
2020-08-18 17:04:39 +0530

    Updates

---
+++ precache-manifest.328593f32f55e4ba4620a7440068be3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f9bc0767fd9fb905bfad41afa5a43a",
+    "url": "/MineSweeper/index.html"
+  },
+  {
+    "revision": "cbf731b41d01f65c2ee8",
+    "url": "/MineSweeper/static/css/main.1b440cc4.chunk.css"
+  },
+  {
+    "revision": "e45892a21473c44558cf",
+    "url": "/MineSweeper/static/js/2.a9e827d9.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/MineSweeper/static/js/2.a9e827d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf731b41d01f65c2ee8",
+    "url": "/MineSweeper/static/js/main.09ff0b40.chunk.js"
+  },
+  {
+    "revision": "df8f29b899b492313345",
+    "url": "/MineSweeper/static/js/runtime-main.edf03609.js"
+  },
+  {
+    "revision": "3f7b5ed00c3aad2c517a32b905090a6f",
+    "url": "/MineSweeper/static/media/increase.3f7b5ed0.png"
+  },
+  {
+    "revision": "b60187c18fc4e071166fd414ed594462",
+    "url": "/MineSweeper/static/media/money.b60187c1.png"
+  }
+]);
